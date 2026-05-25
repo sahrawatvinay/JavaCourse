019f3fc459d26f9d83f5b4332a884d03d8c280a0
Vinay Sahrawat <vinaysahrawat183a@gmail.com>
2026-05-25 20:30:02 +0530

    primitive data types

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    static void main() {
+        //Primitive data types
+        int num1 = 100;
+        byte by = 127; //max range
+        short sh = 515;
+        long lg = 9576746;
+
+        float fl = 50.1f; //always f suffix at end
+        double dbl = 100.8;
+
+        char c = 'x';
+
+        boolean bl = false;
+    }
+}
