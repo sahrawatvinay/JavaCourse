5e37db79b409986a7fe70e600499c8bb0c5f8afa
Vinay Sahrawat <vinaysahrawat183a@gmail.com>
2026-05-30 18:58:07 +0530

    class and object basic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+class Car {
+    String color;
+    int model;
+    void start(){
+        System.out.println("Car started");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Car c = new Car();
+        c.start();
     }
 }
